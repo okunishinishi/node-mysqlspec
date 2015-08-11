72071439aa35f64d95865cc19f1e5e0a121eb98f
Taka Okunishi <okunishinishi@gmail.com>
2015-08-11 12:20:20 +0900

    Version incremented to 1.0.5

--- docs/examples/example-result.js
+++ docs/examples/example-result.js
@@ -1,11 +1,16 @@
-schema = { // Schema for in "my_db.TEST_SHOP" database.
-    name: 'my_db.TEST_SHOP',
-    properties: {
-        id: {type: 'integer', maxLength: 5},
-        article: {type: 'integer', maxLength: 4},
-        dealer: {type: 'string', maxLength: 20},
-        price: {type: 'number'}
+schema = { // Schemas for in "my_db" database.
+    'TEST_SHOP': { // Schema for in "my_db.TEST_SHOP" table.
+        name: 'my_db.TEST_SHOP',
+        properties: {
+            id: {type: 'integer', maxLength: 5},
+            article: {type: 'integer', maxLength: 4},
+            dealer: {type: 'string', maxLength: 20},
+            price: {type: 'number'}
+        },
+        required: ['id', 'article', 'dealer', 'price'],
+        additionalProperties: false
     },
-    required: ['id', 'article', 'dealer', 'price'],
-    additionalProperties: false
+    'TEST_PERSON':{
+        /**...*/
+    }
 };
